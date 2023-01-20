8a7299c5eaf7700ba14b21f2041002d7c285a8e5
Anthony Acabal <atzun-2017067@kinal.edu.gt>
2023-01-20 09:42:11 -0600

    V1 commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+function saludar(){
+    let a = document.getElementById('numA').value;
+    let b = document.getElementById('numB').value;
+
+    let result = parseInt(a)+ parseInt(b);
+
+    console.log('Saludos');
+    console.log(result);
+    
+}
+
